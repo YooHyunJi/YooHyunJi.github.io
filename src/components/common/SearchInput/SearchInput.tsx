4fbed6b8e161bd8f109ccf79fc62055f6b25268c
유현지 <ccamy0915@naver.com>
2024-08-09 15:37:07 +0900

    feat: 검색창, 아이콘 버튼 컴포넌트 생성

---
+++ src/components/common/SearchInput/SearchInput.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export default function SearchInput() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <motion.input
+      initial={{ boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)" }} // 초기 상태
+      animate={{
+        boxShadow: isFocused
+          ? "0px 0px 15px rgba(0, 0, 2, 0.1)" // 포커스 상태의 그림자
+          : "0px 0px 3px rgba(0, 0, 0, 0.25)", // 기본 상태
+      }}
+      type="text"
+      placeholder="뭔가 찾아볼까요?"
+      className="w-[30rem] h-[3.5rem] px-[2rem] py-[1rem] rounded-full focus:outline-none"
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+    />
+  );
+}
