b478773f31c6381650dd04f4dbe854b05d132585
유현지 <ccamy0915@naver.com>
2024-08-28 12:13:57 +0900

    rename: Add backend folder

---
+++ Backend/blog-back/src/main/java/blog/linzy/blogback/board/controller/BoardController.java
@@ -0,0 +1,18 @@
+package blog.linzy.blogback.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+
+    @PostMapping
+    public ResponseEntity<String> createBoard(){
+
+    }
+}
